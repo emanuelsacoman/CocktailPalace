2c2302d115c0c1c25e84b8d084d1494724d4bcaf
EvS444 <emanuelsacoman@gmail.com>
2024-07-04 13:55:36 -0300

    Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './view/index/index.component';
 import { HeadComponent } from './view/head/head.component';
+import { environments } from 'src/environments/environments';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { HeadComponent } from './view/head/head.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environments.firebase)),
   ],
-  providers: [],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: environments.firebase}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
