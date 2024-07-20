f9a0562ae8fcbf4d5d1ea895a14c802f7bfc9e14
EvS444 <emanuelsacoman@gmail.com>
2024-07-20 10:56:56 -0300

    Finalizado tela principal (rushado)

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
+import { CocktailService } from 'src/app/model/services/cocktail.service';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,36 @@ import { Title, Meta } from '@angular/platform-browser';
 export class HomeComponent implements OnInit{
   title = 'Cocktail Palace';
   description = 'Tudo o que você precisa.';
+  backgroundImageUrl1: string | undefined;
+  backgroundImageUrl2: string | undefined;
+  backgroundImageUrl3: string | undefined;
 
-  constructor(private titleService: Title,
-    private metaService: Meta,){
-
-  }
+  constructor(private cocktailService: CocktailService,
+    private titleService: Title,
+    private metaService: Meta) {}
 
   ngOnInit(): void {
     this.setDocTitle(this.title);
     this.setMetaDescription(this.description);
+    this.loadRandomCocktail1()
+    this.loadRandomCocktail2()
+    this.loadRandomCocktail3()
+  }
+
+  loadRandomCocktail1(){
+    this.cocktailService.getRandomCocktail().subscribe(cocktail => {
+      this.backgroundImageUrl1 = cocktail.strDrinkThumb;
+    });
+  }
+  loadRandomCocktail2(){
+    this.cocktailService.getRandomCocktail().subscribe(cocktail => {
+      this.backgroundImageUrl2 = cocktail.strDrinkThumb;
+    });
+  }
+  loadRandomCocktail3(){
+    this.cocktailService.getRandomCocktail().subscribe(cocktail => {
+      this.backgroundImageUrl3 = cocktail.strDrinkThumb;
+    });
   }
 
   setDocTitle(title: string) {
